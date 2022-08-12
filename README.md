fc9bd356015b252b6f5fa7ad2cb3475eb90e0e8a
Yi <myyimail@gmail.com>
2022-08-12 16:20:43 -0400

    yi changes

---
+++ README.md
@@ -0,0 +1,67 @@
+# Twilio Sync JS Quickstart with React
+
+This is an example project for [Twilio Sync](https://www.twilio.com/docs/sync) that uses Javascript and React. 
+
+This application shows you how you might implement cobrowsing in a React application with Twilio Sync. For instance, a customer could be talking to a customer service agent over a voice connection for help filling out a form on a web application. After starting a cobrowsing session, both the agent and the customer can edit and modify the form in real time.
+
+We use Twilio Sync to manage the React application's form data, as well as the list of participants in the cobrowsing session.
+
+You can use this application as the base for your own cobrowsing projects, or you can explore the code in this application to learn more about how to implement solutions with Twilio Sync.
+
+## Prerequisites
+
+You need a recent version of Node.js, as well as a Twilio account.
+
+For more on how to install Node.js, see [How to set up your Node.js Development Environment](https://www.twilio.com/docs/usage/tutorials/how-to-set-up-your-node-js-and-express-development-environment).
+
+If you need to create a Twilio account, you can sign up here: [Try Twilio](https://www.twilio.com/try-twilio).
+
+## Steps to Run the Application
+
+Before you can run the application, you need to fill in your Twilio credentials, and provide a Sync Service id to work with. 
+
+### Create a .env file
+
+Copy the `.env.example` file to a new file named `.env`
+
+### Edit the .env file
+
+Replace the placeholder values in the `.env` file with the appropriate values from your Twilio Console.
+
+| Config Value  | Description |
+| :-------------  |:------------- |
+`TWILIO_ACCOUNT_SID` | Your primary Twilio account identifier - find this [in the console here](https://www.twilio.com/console).
+`TWILIO_API_KEY` | Used to authenticate - [generate one here](https://www.twilio.com/console/dev-tools/api-keys).
+`TWILIO_API_SECRET` | Used to authenticate - [just like the above, you'll get one here](https://www.twilio.com/console/dev-tools/api-keys).
+`TWILIO_SYNC_SERVICE_SID` | Sync Service | [Generate one in the Twilio Sync console](https://www.twilio.com/console/sync/services)
+
+
+### Run `npm install`
+
+Run the `npm install` command to download the Javascript dependencies for the project.
+
+## Running the Quickstart in Development Mode
+
+To run the quickstart in development node, you will need to run two commands, in two different command windows. Run them in this order:
+
+`node server.js`
+
+`npm start`
+
+Watch for any errors in the command line for either one of these applications when they start.
+
+Your web browser will open up to (http://localhost:3000/) with the React app.
+
+Open two tabs, and log in to the cobrowsing session with two different usernames.
+
+Change the form data in one tab, and you will see it change in the other tab.
+
+# Created with Create React App
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Learn More about React and Create React App
+
+You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
+
+To learn React, check out the [React documentation](https://reactjs.org/).
---
+++ src/app/SyncCobrowsing.js
@@ -0,0 +1,282 @@
+import './SyncCobrowsing.css';
+
+import React from 'react';
+import SyncClient from 'twilio-sync';
+import axios from 'axios';
+
+import Participants from './Participants.js'
+import SyncedInputField from './SyncedInputField';
+
+class SyncCobrowsing extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      status: 'Connecting...',
+      errorMessage: '',
+      participants: [],
+      formData: {
+        id: '',
+        petName: '',
+        ownerName: '',
+        aptNotes: '',
+        aptDate: ''
+      }
+    };
+
+    this.setFormValue = this.setFormValue.bind(this);
+  }
+
+  componentDidMount() {
+    // fetch an access token from the localhost server
+    this.retrieveToken(this.props.identity);
+  }
+
+  componentWillUnmount() {
+    this.removeParticipant(this.props.identity);
+  }
+
+  async retrieveToken(identity) {
+    let result = await axios.get('/token/' + this.props.identity);
+    let accessToken = result.data.token;
+    if (accessToken != null) {
+      if (this.client) {
+        // update the sync client with a new access token
+        this.refreshSyncClient(accessToken);
+      } else {
+        // create a new sync client
+        this.createSyncClient(accessToken);
+      }
+    } else {
+      this.setState({'errorMessage':'No access token found in result'});
+    }
+  }
+
+/*   updateSyncDocument(formData) {
+    if (!this.client) {
+      return;
+    }
+    this.client.document(this.props.sessionId).then(function(doc) {
+      doc.set(formData);
+    });
+  } */
+
+  updateSyncListItem(formData) {
+    if (!this.client) {
+      return;
+    }
+    //what happens when there's no list items yet?
+    this.client.list(this.props.sessionId).then(function(list) {
+      list.set(0,formData);
+    });
+  }
+
+  getParticipantsKey() {
+    return 'participants-' + this.props.sessionId;
+  }
+
+  addParticipant(identity) {
+    this.client.map(this.getParticipantsKey()).then(function(map) {
+      map.set(identity, {
+        identity: identity
+      }).then(function(item) {
+        console.log('Added: ', item.key);
+      }).catch(function(err) {
+        console.error(err);
+      });
+    });
+  }
+
+  removeParticipant(identity) {
+    this.client.map(this.getParticipantsKey()).then(function(map) {
+      map.remove(identity)
+        .then(function() {
+          console.log('Participant ' + identity + ' removed.');
+        })
+        .catch(function(error) {
+          console.error('Error removing: ' + identity, error);
+        })
+    });
+  }
+
+  async subscribeToParticipantsUpdates() {
+    var component = this;
+    this.client.map(this.getParticipantsKey()).then(function(map) {
+      map.on('itemAdded', function(event) {
+        component.refreshParticipants(map);
+      });
+
+      map.on('itemUpdated', function(event) {
+        component.refreshParticipants(map);
+      });
+
+      map.on('itemRemoved', function(event) {
+        component.refreshParticipants(map);
+      });
+      
+    });
+  }
+
+  refreshParticipants(map) {
+    this.getAllItems(map).then(items => {
+      var participants = [];
+      items.forEach(item => {
+        participants.push(item.data);
+      });
+      console.log('participants', participants);
+      this.setState({participants: participants});
+    });
+  }
+
+  // Since Sync Map has pagination we need to navigate through all the pages
+  async getAllItems(map) {
+      const result = [];
+      let page = await map.getItems();
+      result.push(...page.items);
+
+      while (page.hasNextPage) {
+          page = await page.nextPage();
+          result.push(...page.items);
+      }
+      return result;
+  };
+
+  refreshSyncClient(token) {
+    this.client.updateToken(token);
+  }
+
+  createSyncClient(token) {
+    const client = new SyncClient(token, { logLevel: 'info' });
+    var component = this;
+    let identity = this.props.identity;
+    client.on('connectionStateChanged', function(state) {
+        if (state === 'connected') {
+            component.client  = client;
+            component.setState({status:'connected'});
+            component.loadFormData();
+            component.subscribeToParticipantsUpdates();
+            component.addParticipant(identity);
+        } else {
+          component.setState({
+            status:'error', 
+            errorMessage:`Error: expected connected status but got ${state}`
+          });
+        }
+    });
+    client.on('tokenAboutToExpire', function() {
+      component.retrieveToken(identity);
+    });
+    client.on('tokenExpired', function() {
+      component.retrieveToken(identity);
+    });
+  }
+
+/*   async loadFormData() {
+    let component = this;
+
+    this.client.document(this.props.sessionId).then(function(doc) {
+      component.setState({formData: doc.data});
+
+      doc.on("updated",function(data) {
+        console.log('Sync Updated Data', data);
+        if (!data.isLocal) {
+          console.log('Setting state with', data.data);
+          component.setState({formData: data.data});
+        }
+      });
+    
+    });
+  } */
+
+  async loadFormData() {
+    let component = this;
+
+    this.client.list(this.props.sessionId).then(function(list) {
+      component.setState({formData: list.get(0)});
+
+      //?????
+      list.on('itemUpdated',function(item) {
+        console.log('Sync Updated Item', item.value);
+        if (!item.isLocal) {
+          console.log('Setting state with', item.value);
+          component.setState({formData: item.value});
+        }
+      });
+    
+    });
+  }
+
+/*   setFormValue(fieldName,value) {
+    var formData = this.state.formData;
+    formData[fieldName] = value;
+    this.setState({formData: formData}, () => this.updateSyncDocument(formData));
+  } */
+
+  setFormValue(fieldName,value) {
+    var formData = this.state.formData;
+    formData[fieldName] = value;
+    this.setState({formData: formData}, () => this.updateSyncListItem(formData));
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container">
+            <div className="card border-primary">
+                <div className="card-header text-info">
+                    <span id="status">{this.state.status}</span>
+                </div>
+                <div className="card-header text-info">
+                    Participants:<br />
+                    <Participants participants={this.state.participants}/>
+                </div>
+            </div>
+            <div className="card border-primary">
+                <div className="card-header text-info">
+                    <div className="input-group mb-3">
+                      <SyncedInputField
+                        setFormValue={this.setFormValue}
+                        formDataKey="id" 
+                        formDataValue={this.state.formData['id']} 
+                        placeholder="ID"/>
+                    </div>
+                    <div className="input-group mb-3">
+                      <SyncedInputField
+                        setFormValue={this.setFormValue}
+                        formDataKey="petName" 
+                        formDataValue={this.state.formData['petName']} 
+                        placeholder="Pet Name"/>
+                    </div>
+                    <div className="input-group mb-3">
+                      <SyncedInputField
+                        setFormValue={this.setFormValue}
+                        formDataKey="ownerName" 
+                        formDataValue={this.state.formData['ownerName']} 
+                        placeholder="Owner Name"/>
+                    </div>
+                    <div className="input-group mb-3">
+                      <SyncedInputField
+                        setFormValue={this.setFormValue}
+                        formDataKey="aptNotes" 
+                        formDataValue={this.state.formData['aptNotes']} 
+                        placeholder="Appointment Notes"/>
+                    </div>
+                    <div className="input-group mb-3">
+                      <SyncedInputField
+                        setFormValue={this.setFormValue}
+                        formDataKey="aptDate" 
+                        formDataValue={this.state.formData['aptDate']} 
+                        placeholder="Appointment Date"/>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <span id="floating-badges"></span>
+        <span id="signals"></span>
+      </React.Fragment>
+    );
+  }
+
+}
+
+export default SyncCobrowsing;
